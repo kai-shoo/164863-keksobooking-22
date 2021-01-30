c707eff2a0fc7117b32be8cf4669a433a20c6cdd
Kai Shoo <qsteamk@gmail.com>
2021-01-30 18:39:57 +0300

    added validation for edge case in RandomizeInRange

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 'use strict';
 
 const randomizeInRange = function (min, max, precision = 0) {
-
   if (
     min >= max ||
     min < 0 ||
-    precision < 0)
+    precision < 0 ||
+    (max - min) < 1 / (10 ** precision))
     return null;
 
   return +(Math.random() * (max - min) + min).toFixed(precision);
 };
 
 randomizeInRange(0.2, 100.99999999, 20);
+
 
 
 
