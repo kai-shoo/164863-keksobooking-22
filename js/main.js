69503a2a9ad4f4e5de905c287b31ae16c3c12263
Kai Shoo <qsteamk@gmail.com>
2021-02-02 14:56:12 +0300

    style fixes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const calcLengthOfFraction = function (number) {
   return number.toString().includes('.')
--- js/test.js
+++ js/test.js
@@ -1,28 +1,25 @@
 'use strict';
 /* eslint-disable */
 
-
-
-
-describe("randomizeInRange", function () {
+describe('randomizeInRange', function () {
 
   function makeTest(min, max, precision) {
     assert.isAtLeast(randomizeInRange(min, max, precision), min);
     assert.isAtMost(randomizeInRange(min, max, precision), max);
   }
 
-  describe("value check  --no-precision", function () {
-    it(`0.1 - 2.9`, function () {
+  describe('value check  --no-precision', function () {
+    it('0.1 - 2.9', function () {
       for (let x = 1; x <= 50; x++) {
         makeTest(0.1, 2.9);
       }
     });
-    it(`0.1 - 1.5`, function () {
+    it('0.1 - 1.5', function () {
       for (let x = 1; x <= 50; x++) {
         makeTest(0.1, 1.5);
       }
     });
-    it(`0, 100`, function () {
+    it('0, 100', function () {
       for (let x = 1; x <= 50; x++) {
         makeTest(0, 100);
       }
@@ -30,38 +27,38 @@ describe("randomizeInRange", function () {
 
   })
 
-  describe("value check  --precision", function () {
-    it(`1.1, 1.5, 1`, function () {
+  describe('value check  --precision', function () {
+    it('1.1, 1.5, 1', function () {
       for (let x = 1; x <= 50; x++) {
         makeTest(1.1, 1.5, 1);
       }
     });
 
-    it(`1, 1.4, 2)`, function () {
+    it('1, 1.4, 2)', function () {
       for (let x = 1; x <= 50; x++) {
         makeTest(1, 1.4, 2);
       }
     });
   });
 
-  describe("value check  --precision", function () {
+  describe('value check  --precision', function () {
 
   });
 
-  describe("null check", function () {
-    it(`1.1, 1.4, 0`, function () {
+  describe('null check', function () {
+    it('1.1, 1.4, 0', function () {
       for (let x = 1; x <= 50; x++) {
         assert.equal(randomizeInRange(1.1, 1.4, 0), null);
       }
     });
 
-    it(`-1, 2.4, 2)`, function () {
+    it('-1, 2.4, 2)', function () {
       for (let x = 1; x <= 50; x++) {
         assert.equal(randomizeInRange(-1, 2.4, 2), null);
       }
     });
 
-    it(`1.1, 1.4, 0.5)`, function () {
+    it('1.1, 1.4, 0.5)', function () {
       for (let x = 1; x <= 50; x++) {
         assert.equal(randomizeInRange(1.1, 1.4, 0.5), null);
       }
