56c52c6bffcf54c8685dc0b1f4a49f320d48b1dd
Kai Shoo <qsteamk@gmail.com>
2021-02-25 17:37:47 +0300

    fix style

--- js/util.js
+++ js/util.js
@@ -1,3 +1,5 @@
+/*eslint-disable  indent*/
+
 const TIMEOUT_SEC = 10;
 
 const keepElementsByClassFromArr = function (array, elements) {
@@ -25,9 +27,9 @@ const timeout = function (s) {
 const AJAX = async function (url, uploadData = undefined) {
   const fetchPro = uploadData
     ? fetch(url, {
-      method: 'POST',
-      body: uploadData,
-    })
+        method: 'POST',
+        body: uploadData,
+      })
     : fetch(url);
 
   const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
